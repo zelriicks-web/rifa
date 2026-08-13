70c57da8b7e18d93ea03d80eb7d1ea1ad68a3312
zelriicks-web <zelriicks@gmail.com>
2026-08-13 15:00:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def inicio():
-    return render_template("index.html")
+    numeros = []
+
+    for numero in range(1, 101):
+        numeros.append(f"{numero:04d}")
+
+    return render_template(
+        "index.html",
+        numeros=numeros
+    )
 
 
 if __name__ == "__main__":
